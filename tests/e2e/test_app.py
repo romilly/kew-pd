9ab5ac65d023e552019b88a61fecaab95fb8fbe8
Romilly Cocking <romilly.cocking@gmail.com>
2024-01-07 11:18:44 +0000

    Re-installing requirements + dealing with cruft from project name change

--- tests/e2e/test_app.py
+++ tests/e2e/test_app.py
@@ -4,7 +4,7 @@ import streamlit.testing.v1 as sat
 
 
 def test_planets():
-    app = sat.AppTest.from_file("/home/romilly/git/active/kw-pet/src/pew.py")
+    app = sat.AppTest.from_file("/home/romilly/git/active/kew-pd/src/pew.py")
     app.run(timeout=60)
     ci = app.chat_input("chat_input").set_value('Name the planets of the solar system').run(timeout=60)
     cms = app.get("chat_message")
